a4534f318a32931e3e7d1c3cfbf96b5452222a5e
Sam Deters <sjdeters@unc.edu>
2023-10-05 22:10:33 -0400

    EX04 v2

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,9 +1,12 @@
-"""EX04 - 'list' Utility Functions"""
+"""EX04 - 'list' Utility Functions."""
 
 __author__ = "730660578"
 
+
 def all(input_list: list[int], input_int: int) -> bool:
     """Determines if a given intger is seen in every number of a list."""
+    if len(input_list) == 0:
+        return False
     idx: int = 0
     while idx < len(input_list) - 1:
         if input_list[idx] != input_int:
@@ -12,8 +15,9 @@ def all(input_list: list[int], input_int: int) -> bool:
             idx += 1
     return True
 
+
 def max(input: list[int]) -> int:
-    """Returns the largest value in the list"""
+    """Returns the largest value in the list."""
     if len(input) == 0:
         raise ValueError("max() arg is an empty list")
     idx: int = 0
@@ -24,8 +28,11 @@ def max(input: list[int]) -> int:
         idx += 1
     return largest_int
 
+
 def is_equal(int_list1: list[int], int_list2: list[int]) -> bool:
-    """Determines if both lists are the same"""
+    """Determines if both lists are the same."""
+    if len(int_list1) == 0 or len(int_list2) == 0:
+        raise ValueError("is_equal() arg list one or two is empty")
     if len(int_list1) != len(int_list2):
         raise ValueError("is_equal() arg lists are not equal length")
     idx: int = 0
